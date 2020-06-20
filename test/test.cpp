320f846da92425add110c8c7416cf81250b8871e
Anton Skochko <antonskochko@gmail.com>
2020-06-20 21:54:53 +0300

    basic testing

---
+++ test/test.cpp
@@ -0,0 +1,80 @@
+#include <sstream>
+#include <vector>
+#include <string>
+#include <cassert>
+#include <set>
+#include <unordered_map>
+
+#include "inidoc.hpp"
+#include "utils.hpp"
+
+const char* const ini_str =
+R"(`; comment
+# unix-style comment
+
+[Section1]
+; setcion comment
+var1=значение_1 ; parameter commment
+var2=значение_2
+  
+[Section2]
+var1=значение_1
+var2=значение_2
+
+; multiple coma separated params
+[Section3]
+var1=значение_1_1, значение_1_2, значение_1_3
+var2=значение_2
+; Zend framework list of params
+[Section3.1]
+var1[]=значение_1_1
+var1[]=значение_1_2
+var1[]=значение_1_3
+var2=значение_2
+
+; Empty params
+[Section4.0]
+[ViewState]
+Mode=
+Vid=
+FolderType=Generic
+)";
+
+void test_add_section()
+{
+    inidoc doc;
+    doc.AddSection("Section 1");
+    doc.AddSection("Section 2");
+    doc.AddSection("Section 3");
+    doc.AddSection("Section 3");
+    doc.AddSection("Section 2");
+    std::set<std::string> expected_sections = {"Section 1", "Section 2", "Section 3"};
+    assert(doc.GetSectionNames() == expected_sections);
+}
+
+void test_add_params()
+{
+    inidoc doc;
+    
+    doc.AddSection("Section 1");
+    doc.AddParam("Section 1", "One", "Two");
+    doc.AddParam("Section 1", "Two", "Three");
+
+    std::unordered_map<std::string, std::string> params = {
+        {"One", "Two"},
+        {"Two", "Three"},
+    };
+
+    auto section = doc.GetSection("Section 1");
+    assert(section == params);
+}
+
+int main() {
+    try {
+        test_add_section();
+        test_add_params();
+        std::cout << "All tests passed\n";
+    } catch (std::exception) {
+        std::cout << "Failed to test parsini\n";
+    }
+}
